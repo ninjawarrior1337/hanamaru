ddee6e781202572df99ef9081b61b75e67fc45f9
ninjawarrior1337 <teelar1337@gmail.com>
2019-11-26 01:18:30 -0800

    set owner id by env

--- src/main/kotlin/xyz/treelar/hanamaru/CommandController.kt
+++ src/main/kotlin/xyz/treelar/hanamaru/CommandController.kt
@@ -3,6 +3,7 @@ package xyz.treelar.hanamaru
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandClient
 import com.jagrosh.jdautilities.command.CommandClientBuilder
+import io.github.cdimascio.dotenv.dotenv
 import net.dv8tion.jda.api.entities.Activity
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -11,6 +12,7 @@ import javax.annotation.PostConstruct
 @Component
 class CommandController
 {
+    final val dotenv = dotenv()
     val commandBuilder = CommandClientBuilder()
 
     lateinit var commandClient: CommandClient
@@ -21,7 +23,7 @@ class CommandController
     @PostConstruct
     fun build() {
         commandBuilder.addCommands(*commands)
-        commandBuilder.setOwnerId("102939281470816256")
+        commandBuilder.setOwnerId(dotenv["OWNER"])
         commandBuilder.setPrefix("*")
         commandBuilder.setActivity(Activity.playing("LLSIFAS"))
         commandClient = commandBuilder.build()
