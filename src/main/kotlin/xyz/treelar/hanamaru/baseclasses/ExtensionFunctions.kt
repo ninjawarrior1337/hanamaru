037e417cba5c315db6c48e9ac5a2a584edc8c5ff
ninjawarrior1337 <teelar1337@gmail.com>
2019-11-28 00:12:53 -0800

    Add avatar

--- src/main/kotlin/xyz/treelar/hanamaru/baseclasses/ExtensionFunctions.kt
+++ src/main/kotlin/xyz/treelar/hanamaru/baseclasses/ExtensionFunctions.kt
@@ -1,7 +1,23 @@
 package xyz.treelar.hanamaru.baseclasses
 
+import com.jagrosh.jdautilities.command.CommandEvent
 import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.entities.User
 
 fun MessageChannel.sendAndQueue(message: CharSequence) {
     this.sendMessage(message).queue()
+}
+
+fun CommandEvent.getUserParam(): User? {
+    return when {
+        this.message.mentionedUsers.getOrNull(0) != null -> {
+            message.mentionedUsers[0]
+        }
+        this.args.isNotEmpty() -> {
+            jda.getUserById(this.args)
+        }
+        else -> {
+            this.author
+        }
+    }
 }
--- src/main/kotlin/xyz/treelar/hanamaru/commands/utils/Avatar.kt
+++ src/main/kotlin/xyz/treelar/hanamaru/commands/utils/Avatar.kt
@@ -1,6 +1,19 @@
 package xyz.treelar.hanamaru.commands.utils
 
-class Avatar
-{
+import com.jagrosh.jdautilities.command.Command
+import com.jagrosh.jdautilities.command.CommandEvent
+import org.springframework.stereotype.Component
+import xyz.treelar.hanamaru.baseclasses.getUserParam
 
+@Component
+class Avatar: Command()
+{
+    init {
+        name = "avatar"
+    }
+    override fun execute(event: CommandEvent) {
+        event.getUserParam()?.run {
+            event.reply(this.avatarUrl)
+        }
+    }
 }
